41826ab9ec20d69e15a676359d0e19ab260c8beb
sheffykhlg <56907336+sheffykhlg@users.noreply.github.com>
2026-04-24 20:54:42 +0530

    Update referral.py

--- SAMRABOTZ_PLUGIN/referral.py
+++ SAMRABOTZ_PLUGIN/referral.py
@@ -1,4 +1,5 @@
 from pyrogram import Client, filters
+from config import Config, admin_states
 from database import db
 from utils import ref_keyboard
 @Client.on_message(filters.command(["referral", "refferal"]) & filters.private)
@@ -12,7 +13,6 @@ async def referral_cmd(client, message):
     await message.reply(f"👥 <b>Refer</b>\n\n{config.get('ref_text', '')}\n\n🔗 <code>{ref_link}</code>\n\n🪙 Points: {user['ref_balance']}/{config['ref_count']}", reply_markup=ref_keyboard(), disable_web_page_preview=True)
 @Client.on_message(filters.command("ref") & filters.user(Config.ADMIN_IDS))
 async def toggle_ref(client, message):
-    from config import admin_states
     if len(message.command) < 2: return
     if message.command[1].lower() == "off":
         await db.update_settings({"ref_system": False})
