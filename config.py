8879dcb7fc0c0af34dccc95e7e055cbe0f40ec21
sheffykhlg <56907336+sheffykhlg@users.noreply.github.com>
2026-05-19 15:13:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -55,36 +55,115 @@ class Config:
     MUTE_PENALTY_MINUTES = 2
 
     ADJECTIVES = [
-        "Desi", "Punjab", "Mumbai", "Delhi", "Royal", "Gabru",
-        "Jhakaas", "Bindaas", "Nawab", "Badshah", "Sultan",
-        "Pataka", "Shikari", "Toofan", "Jatt", "Sarpanch",
-        "Khalnayak", "Sherdil", "Dabang", "Raftaar", "Ziddi",
-        "Gabbar", "Veer", "Yoddha", "Singham", "Bhau",
-        "Tapori", "Mast", "Dhakad", "Chulbul", "Akhanda",
-        "Rangeela", "Jhakkas", "Lafandar", "Teekha", "Mirchi",
-        "Baazigar", "Junglee", "Tashan", "Bindass",
-        "Rangbaaz", "Befikra", "Diler", "Hindustani",
-        "Patiala", "Ludhiana", "Haryana", "Rajasthani",
-        "Bambaiya", "Banarasi", "Lucknawi"
-    ]
+    "Desi", "Punjab", "Mumbai", "Delhi", "Royal", "Gabru",
+    "Jhakaas", "Bindaas", "Nawab", "Badshah", "Sultan",
+    "Pataka", "Shikari", "Toofan", "Jatt", "Sarpanch",
+    "Khalnayak", "Sherdil", "Dabang", "Raftaar", "Ziddi",
+    "Gabbar", "Veer", "Yoddha", "Singham", "Bhau",
+    "Tapori", "Mast", "Dhakad", "Chulbul", "Akhanda",
+    "Rangeela", "Jhakkas", "Lafandar", "Teekha", "Mirchi",
+    "Baazigar", "Junglee", "Tashan", "Bindass",
+    "Rangbaaz", "Befikra", "Diler", "Hindustani",
+    "Patiala", "Ludhiana", "Haryana", "Rajasthani",
+    "Bambaiya", "Banarasi", "Lucknawi",
+
+    # Punjabi
+    "Sidhu", "Sandhu", "Gill", "Brar", "Cheema",
+    "Dhillon", "Maan", "Grewal", "Bains", "Sekhon",
+    "Randhawa", "Pannu", "Bajwa", "Aulakh", "Waraich",
+    "Khalsa", "Majhail", "Doaba", "Malwa", "Pindwala",
+    "TruckanWala", "BulletWala", "Patialashahi",
+    "PaggWala", "Sardara", "Gabroo", "Jigrewala",
+    "PindanWala", "CanadaWala", "MohaliWala",
+    "ChandigarhWala", "Taurwala", "Kabza", "Gedi",
+    "FanneyKhan", "Attwadi", "Yamla", "Pendu",
+    "JattLife", "Kaim", "Velly", "Zaildar",
+    "ProudPunjabi", "JattDa", "Nashedi", "LaalPari",
+    "Pistol", "315Wala", "Dunali", "TopBoy",
+    "Mucchad", "Fukra", "Asla", "Rakaane",
+    "PindKing", "Ghaint", "Jazbati", "Legend",
+    "TibeyanDa", "FordWala", "ScorpioWala",
+
+    # Hindi / North Indian
+    "Pandey", "Tiwari", "Sharma", "Dubey", "Yadav",
+    "Rajput", "Thakur", "Chauhan", "Sisodiya",
+    "Rathore", "Tanwar", "Solanki", "Chandel",
+    "Maurya", "Bisht", "Negi", "Rawat",
+    "Bihari", "UPWala", "Haryanvi", "Desiwala",
+    "Kanpuriya", "Allahabadi", "Banarsi",
+    "Jaipuri", "Bhopali", "Lucknowi", "Dehati",
+    "Kattar", "Diler", "Bhaukaal", "Aashiq",
+    "Tapasya", "NetaJi", "Khiladi", "Chora",
+    "Tau", "Khandani", "Sanskari", "Nawabi",
+    "Badmash", "Lathmaar", "Berozgaar", "GymBoy",
+    "Kamina", "Aawara", "HeroNo1", "Chhapri",
+    "DilerBoy", "MastMoula", "Gunda", "Kaleshi",
+
+    # Extra Stylish / Attitude
+    "Danger", "Monster", "Psycho", "Savage",
+    "King", "Boss", "Emperor", "Mafia",
+    "Sniper", "Hunter", "Killer", "Rider",
+    "NoFear", "Fire", "Storm", "Thunder",
+    "Dark", "Shadow", "Ghost", "Venom",
+    "Alpha", "Wolf", "LionHeart", "Beast",
+    "Turbo", "Nitro", "Diesel", "Petrolhead",
+    "Speedy", "Roadster", "StreetKing",
+    "Attitude", "RoyalBlood", "Heartless",
+    "BadBoy", "Wanted", "Rowdy", "Khatarnak"
+]
 
     NOUNS = [
-        "Jatt", "Gabru", "Sher", "Baaz", "Yoddha",
-        "Rakshak", "Toofan", "Aatank", "Pataka",
-        "Sultan", "Badshah", "Nawab", "Bhau",
-        "Tapori", "Daku", "Khalnayak", "Mirchi",
-        "Raftaar", "Bijli", "Bullet", "Shera",
-        "Gabbar", "Don", "Sarpanch", "Launda",
-        "Munda", "Veer", "Pahalwan", "Fauji",
-        "Sipahi", "Raja", "Maharaja", "Thakur",
-        "Pandit", "Chaudhary", "Patel", "Naik",
-        "Banda", "Tiger", "Singham", "Baazigar",
-        "Rider", "Driver", "Raider", "Player",
-        "Mastana", "Bawandar", "Diler",
-        "Rangebaaz", "Lafandar", "Jhakkas"
-    ]
+    "Jatt", "Gabru", "Sher", "Baaz", "Yoddha",
+    "Rakshak", "Toofan", "Aatank", "Pataka",
+    "Sultan", "Badshah", "Nawab", "Bhau",
+    "Tapori", "Daku", "Khalnayak", "Mirchi",
+    "Raftaar", "Bijli", "Bullet", "Shera",
+    "Gabbar", "Don", "Sarpanch", "Launda",
+    "Munda", "Veer", "Pahalwan", "Fauji",
+    "Sipahi", "Raja", "Maharaja", "Thakur",
+    "Pandit", "Chaudhary", "Patel", "Naik",
+    "Banda", "Tiger", "Singham", "Baazigar",
+    "Rider", "Driver", "Raider", "Player",
+    "Mastana", "Bawandar", "Diler",
+    "Rangebaaz", "Lafandar", "Jhakkas",
 
-    ADMIN_GOD_NAME = "🜲 𝕲𝖔𝖉 𝕺𝖋 𝕭𝖔𝖙 🜲"
+    # Punjabi Style
+    "Sardaar", "Khalsa", "Pagg", "Punjabiyan",
+    "Majhail", "Doabia", "Malwai", "PindBoy",
+    "TruckDriver", "BulletRider", "Jigrebaaz",
+    "SherPunjab", "JattDaMunda", "PindKing",
+    "PatialaPeg", "GabrooSher", "Kisaan",
+    "Zamindar", "Nihang", "Faujan",
+    "AslaBoy", "FordBoy", "ScorpioKing",
+    "GediRoute", "KabzaKing", "TaurMunda",
+    "CanadaReturn", "PindAala", "PenduJatt",
+    "Mucchad", "DunaliBoy", "315Gang",
+    "GhaintMunda", "JattLife", "KaimSardaar",
+
+    # Hindi / Desi
+    "Rajput", "Yadav", "PandeyJi", "Brahman",
+    "Bhaukaal", "Neta", "Dabangg", "Badmash",
+    "Chora", "Tau", "Bhai", "Khiladi",
+    "Nawabi", "DesiBoy", "Hindustani",
+    "Jungbaaz", "Kattar", "RickshawKing",
+    "RoadKing", "DesiMunda", "Hero",
+    "Gunda", "Lathait", "GymLover",
+    "Aashiq", "Dilwala", "ChhapriKing",
+    "KaleshBoy", "SanskariBoy", "KaminaBoy",
+
+    # Stylish / Gaming / Internet
+    "Beast", "Monster", "Hunter", "Sniper",
+    "Killer", "Warrior", "Legend", "Shadow",
+    "Ghost", "Venom", "Wolf", "Alpha",
+    "Mafia", "Boss", "King", "Emperor",
+    "Roadster", "StreetKing", "TurboRider",
+    "NitroKing", "DarkKnight", "FireSoul",
+    "StormBreaker", "ThunderBoy", "SavageBoy",
+    "NoFear", "Heartless", "PsychoBoy",
+    "DangerBoy", "WantedMunda", "RowdyKing"
+]
+
+    ADMIN_GOD_NAME = "😈 DEVIL GOD 😈"
 
     if BOT_TOKEN == "8660092184:AAEBYIU6lBaVvS8M6MK372UU9qDCExDNYAM":
         logger.warning(
